bd7d0b1462a71a40dd2ae69e0c4a17b8cb7db178
Ritesh Kumar <44864954+ritshkr1@users.noreply.github.com>
2023-06-03 13:17:12 +0530

    15v

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
-import { BrowserModule , Title } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { SweetAlert2Module } from '@toverux/ngx-sweetalert2';
+import { BrowserModule, Title } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { AngularFontAwesomeModule } from "angular-font-awesome";
+// import { SweetAlert2Module } from '@toverux/ngx-sweetalert2';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/app.component';
-import { MoviesComponent } from './components/list-movies/movies.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./components/app.component";
+import { MoviesComponent } from "./components/list-movies/movies.component";
 
-import { MoviesService } from './services/movies.service';
-import { AddMovieComponent } from './components/add-movie/add-movie.component';
-import { EditMovieComponent } from './components/edit-movie/edit-movie.component';
-import { QualitiesService } from './services/qualities.service';
+import { MoviesService } from "./services/movies.service";
+import { AddMovieComponent } from "./components/add-movie/add-movie.component";
+import { EditMovieComponent } from "./components/edit-movie/edit-movie.component";
+import { QualitiesService } from "./services/qualities.service";
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
     AddMovieComponent,
-    EditMovieComponent
+    EditMovieComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,12 +29,9 @@ import { QualitiesService } from './services/qualities.service';
     BrowserModule,
     FormsModule,
     RouterModule,
-    SweetAlert2Module.forRoot()
+    // SweetAlert2Module.forRoot()
   ],
-  providers: [
-    MoviesService,
-    QualitiesService
-  ],
-  bootstrap: [AppComponent]
+  providers: [MoviesService, QualitiesService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
